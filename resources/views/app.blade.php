406b15fd0d37acb3bf72fe634b9aa0ac5c366d2d
hkovesdi <kovesdi.hunor99@gmail.com>
2020-05-17 00:05:03 +0200

    added material design

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,18 +2,31 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta content="initial-scale=1, shrink-to-fit=no, width=device-width" name="viewport">
 
         <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+            <!-- CSS -->
+        <!-- Add Material font (Roboto) and Material icon as needed -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Roboto+Mono:300,400,700|Roboto+Slab:300,400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+        <!-- Add Material CSS, replace Bootstrap CSS -->
+        <link href="css/material.min.css" rel="stylesheet">
+    
         <script
             src="https://code.jquery.com/jquery-3.5.1.min.js"
             integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
             crossorigin="anonymous">
         </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+        <!-- Then Material JavaScript on top of Bootstrap JavaScript -->
+        <script src="js/material.min.js"></script>
+
         <script type="text/javascript">
             const Toast = Swal.mixin({
                 toast: true,
@@ -32,7 +45,6 @@
         <style>
             html, body {
                 background-color: #6699ff;
-                color: white;
                 font-family: "Roboto", sans-serif;
                 font-weight: 250;
                 height: 100vh;
