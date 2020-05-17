009408e6d575e95e68bda068f2296f1a19bc49ef
Divx1087 <ultimateee1087@gmail.com>
2020-05-18 01:35:27 +0200

    responsive table etc

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,26 +1,38 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta content="initial-scale=1, shrink-to-fit=no, width=device-width" name="viewport">
+        <meta charset="utf-8" />
+        <meta
+            content="initial-scale=1, shrink-to-fit=no, width=device-width"
+            name="viewport"
+        />
 
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-            <!-- CSS -->
+        <link
+            href="https://fonts.googleapis.com/css?family=Nunito:200,600"
+            rel="stylesheet"
+        />
+        <!-- CSS -->
         <!-- Add Material font (Roboto) and Material icon as needed -->
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Roboto+Mono:300,400,700|Roboto+Slab:300,400,700" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link
+            href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Roboto+Mono:300,400,700|Roboto+Slab:300,400,700"
+            rel="stylesheet"
+        />
+        <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+        />
 
         <!-- Add Material CSS, replace Bootstrap CSS -->
-        <link href="css/material.min.css" rel="stylesheet">
-    
+        <link href="css/material.min.css" rel="stylesheet" />
+
         <script
             src="https://code.jquery.com/jquery-3.5.1.min.js"
             integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-            crossorigin="anonymous">
-        </script>
+            crossorigin="anonymous"
+        ></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
@@ -30,20 +42,21 @@
         <script type="text/javascript">
             const Toast = Swal.mixin({
                 toast: true,
-                position: 'top-end',
+                position: "top-end",
                 showConfirmButton: false,
                 timer: 3000,
                 timerProgressBar: true,
-                onOpen: (toast) => {
-                    toast.addEventListener('mouseenter', Swal.stopTimer)
-                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                onOpen: toast => {
+                    toast.addEventListener("mouseenter", Swal.stopTimer);
+                    toast.addEventListener("mouseleave", Swal.resumeTimer);
                 }
             });
         </script>
 
         <!-- Styles -->
         <style>
-            html, body {
+            html,
+            body {
                 background-color: #6699ff;
                 font-family: "Roboto", sans-serif;
                 font-weight: 250;
@@ -84,28 +97,27 @@
                 padding: 0 25px;
                 font-size: 13px;
                 font-weight: 600;
-                letter-spacing: .1rem;
+                letter-spacing: 0.1rem;
                 text-decoration: none;
                 text-transform: uppercase;
             }
 
-            .mwelcome {
-                margin:30px 0px 30px;
-            }
+            /* login page */
 
             .form {
                 border-radius: 7px;
                 position: relative;
                 z-index: 1;
-                background: #FFFFFF;
+                background: #ffffff;
                 max-width: 360px;
                 margin: 0 auto 100px;
                 padding: 30px;
                 text-align: center;
                 border: 0;
-                box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+                box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2),
+                    0 5px 5px 0 rgba(0, 0, 0, 0.24);
             }
-            
+
             .login-page {
                 width: 360px;
                 padding: 8% 0 0;
@@ -122,8 +134,8 @@
                 padding: 15px;
                 box-sizing: border-box;
                 font-size: 14px;
-                box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
-                
+                box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2),
+                    0 5px 5px 0 rgba(0, 0, 0, 0.24);
             }
 
             .loginbutton {
@@ -135,16 +147,19 @@
                 width: 50%;
                 border: 0;
                 padding: 15px;
-                color: #FFFFFF;
+                color: #ffffff;
                 font-size: 14px;
                 font-weight: bold;
                 -webkit-transition: all 0.3 ease;
                 transition: all 0.3 ease;
                 cursor: pointer;
-                box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+                box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2),
+                    0 5px 5px 0 rgba(0, 0, 0, 0.24);
             }
 
-            .form button:hover,.form button:active,.form button:focus {
+            .loginbutton:hover,
+            .loginbutton:active,
+            .loginbutton:focus {
                 background: #4d88ff;
             }
 
@@ -165,6 +180,8 @@
             input:focus {
                 border: 1px solid #6699ff;
             }
+
+            /* menu */
 
             .topnav ul {
                 list-style-type: none;
@@ -197,7 +214,7 @@
                 background-color: #6699ff;
             }
 
-            .topnav ul li a:hover{
+            .topnav ul li a:hover {
                 background-color: #111111;
             }
 
@@ -205,8 +222,10 @@
                 float: right;
             }
 
+            /* menu resp */
+
             @media screen and (max-width: 600px) {
-                .topnav ul li.right, 
+                .topnav ul li.right,
                 .topnav ul li,
                 .topnav ul li.dropbtn {
                     float: none;
@@ -223,7 +242,8 @@
                 text-decoration: none;
             }
 
-            li a:hover, .dropdown:hover .dropbtn {
+            li a:hover,
+            .dropdown:hover .dropbtn {
                 background-color: #111;
             }
 
@@ -236,7 +256,7 @@
                 position: absolute;
                 background-color: #f9f9f9;
                 min-width: 160px;
-                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+                box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
                 z-index: 1;
             }
 
@@ -261,53 +281,211 @@
                 display: block;
             }
 
-            .show {display: block;}
-            
+            .show {
+                display: block;
+            }
+
+            /* table */
+
+            table { 
+                width: 98%;
+                margin-top: 20px;
+                border-collapse: collapse;
+            }
+
+            tr { 
+                background: #eee; 
+            }
+
+            .fejlec-center {
+                text-align: center;
+                vertical-align: middle;
+            }
+
+            th { 
+                background: #333; 
+                color: white; 
+                font-weight: bold; 
+                text-align: center; 
+            }
+
+            td {
+                text-align: left; 
+            }
+
+            td, th { 
+                padding: 6px; 
+                border: 1px solid #ccc; 
+            }
+
+            .centercell {
+                text-align: center;
+                vertical-align: middle;
+                 
+            }
+
+            .fizetve-button {
+                border: 1px solid white;
+                font-family: "Roboto", sans-serif;
+                text-transform: uppercase;
+                background: #6699ff;
+                color: #ffffff;
+                font-size: 14px;
+                font-weight: bold;
+                cursor: pointer;
+                box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2),
+                    0 5px 5px 0 rgba(0, 0, 0, 0.24);
+            }
+
+            .check {
+                box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2),
+                    0 5px 5px 0 rgba(0, 0, 0, 0.24);
+            }
+
+            .fizetve-button:hover,
+            .fizetve-button:active,
+            .fizetve-button:focus {
+                background: #4d88ff;
+            }
+
+            /* table resp */
+
+            @media only screen and (max-width: 600px) {
+                table,
+                thead,
+                tbody,
+                th,
+                td,
+                tr {
+                    display: block;
+                }
+
+                table {
+                    margin-top: 160px;
+                    width: 95%;
+                }
+
+                thead tr {
+                    position: absolute;
+                    top: -9999px;
+                    left: -9999px;
+                }
+
+                tr {
+                    margin: 0 0 1rem 0;
+                    background: #eee;
+                }
+
+                td {
+                    border: 1px solid #ccc;
+                    border-bottom: 0;
+                    position: relative;
+                    padding-left: 50%;
+                    text-align: center;
+                }
+
+                td:before {
+                    position: absolute;
+                    left: 6px;
+                    width: 45%;
+                    padding-right: 10px;
+                    white-space: nowrap;
+                }
+
+                td:nth-of-type(1):before {
+                    content: "ID";
+                }
+
+                td:nth-of-type(2):before {
+                    content: "Név";
+                }
+
+                td:nth-of-type(3):before {
+                    content: "Cim";
+                }
+
+                td:nth-of-type(4):before {
+                    content: "Tel";
+                }
+
+                td:nth-of-type(5):before {
+                    content: "Készpénz";
+                }
+
+                td:nth-of-type(6):before {
+                    content: "Bankkártya";
+                }
+
+                td:nth-of-type(7):before {
+                    content: "Szépkártya";
+                }
+
+                td:nth-of-type(8):before {
+                    content: "Baptista";
+                }
+
+                td:nth-of-type(9):before {
+                    content: "Tartozás";
+                }
+
+                td:nth-of-type(10):before {
+                    content: "Fizetett";
+                }
+
+            }
+
         </style>
     </head>
     <body>
         @if (\Session::has('success'))
-            <script type="text/javascript">
-                const success = <?php echo json_encode(\Session::get('success')) ?>;
-                success.forEach((msg) => {
-                    Toast.fire({
-                        icon: 'success',
-                        title: msg
-                    });
+        <script type="text/javascript">
+            const success = <?php echo json_encode(\Session::get('success')) ?>;
+            success.forEach((msg) => {
+                Toast.fire({
+                    icon: 'success',
+                    title: msg
                 });
-            </script>
+            });
+        </script>
         @elseif(\Session::has('failure'))
-            <script type="text/javascript">
-                const failure = <?php echo json_encode(\Session::get('failure')) ?>;
-                failure.forEach((msg) => {
-                    Toast.fire({
-                        icon: 'error',
-                        title: msg
-                    });
+        <script type="text/javascript">
+            const failure = <?php echo json_encode(\Session::get('failure')) ?>;
+            failure.forEach((msg) => {
+                Toast.fire({
+                    icon: 'error',
+                    title: msg
                 });
+            });
+        </script>
+        @endif @section('content')
 
-            </script>
-        @endif
-       @section('content')
-
-       <script>
+        <script>
             function myFunction() {
                 document.getElementById("myDropdown").classList.toggle("show");
             }
 
             // Close the dropdown if the user clicks outside of it
             window.onclick = function(event) {
-            if (!event.target.matches('.dropbtn')) {
-                var dropdowns = document.getElementsByClassName("dropdown-content");
-                var i;
-                for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
+                if (!event.target.matches(".dropbtn")) {
+                    var dropdowns = document.getElementsByClassName(
+                        "dropdown-content"
+                    );
+                    var i;
+                    for (i = 0; i < dropdowns.length; i++) {
+                        var openDropdown = dropdowns[i];
+                        if (openDropdown.classList.contains("show")) {
+                            openDropdown.classList.remove("show");
+                        }
+                    }
                 }
-                }
-            }
-            }
+            };
+        </script>
+
+        <script>
+            $("input:checkbox").click(function() {
+            var bol = $("input:checkbox:checked").length >= 1;     
+            $("input:checkbox").not(":checked").attr("disabled",bol);
+            });
         </script>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,133 @@
-
-@extends('app')
-
-@section('content')
+@extends('app') @section('content')
 
 <body>
-    <nav class="topnav" >
-            <ul>
-                <li><a class="active" href="#home">Főoldal</a></li>
-                <li onclick="myFunction()" class="dropdown"><a href="javascript:void(0)" class="dropbtn">Futárok</a>
-                    <div id="myDropdown" class="dropdown-content">
-                        <a href="#1">Futar1</a>
-                        <a href="#2">Futar2</a>
-                        <a href="#3">Futar3</a>
-                    </div>
-                </li>
-                <li><a href="#diagrams">Diagramok</a></li>
-                <li class="right"><a href="#logout">Kijelentkezés</a></li>
-            </ul>
-</nav>
+    <nav class="topnav">
+        <ul>
+            <li><a class="active" href="#home">Főoldal</a></li>
+            <li onclick="myFunction()" class="dropdown">
+                <a href="javascript:void(0)" class="dropbtn">Futárok</a>
+                <div id="myDropdown" class="dropdown-content">
+                    <a href="#1">Futar1</a>
+                    <a href="#2">Futar2</a>
+                    <a href="#3">Futar3</a>
+                    <a href="#4">Futar4</a>
+                    <a href="#5">Futar5</a>
+                    <a href="#6">Futar6</a>
+                    <a href="#7">Futar7</a>
+                </div>
+            </li>
+            <li><a href="#diagrams">Diagramok</a></li>
+            <li class="right"><a href="#logout">Kijelentkezés</a></li>
+        </ul>
+    </nav>
+    <div class="flex-center">
+    <table role="table">
+        <thead role="rowgroup">
+            <tr role="row">
+                <th role="columnheader" class="fejlec-center">ID</th>
+                <th role="columnheader" class="fejlec-center">Név</th>
+                <th role="columnheader" class="fejlec-center">Cim</th>
+                <th role="columnheader" class="fejlec-center">Tel</th>
+                <th role="columnheader" class="fejlec-center">Készpénz</th>
+                <th role="columnheader" class="fejlec-center">Bankkártya</th>
+                <th role="columnheader" class="fejlec-center">Szépkártya</th>
+                <th role="columnheader" class="fejlec-center">Baptista</th>
+                <th role="columnheader" class="fejlec-center">Tartozás</th>
+                <th role="columnheader" class="fejlec-center">Fizetett</th>
+            </tr>
+        </thead>
+        <tbody role="rowgroup">
+            <tr role="row">
+                <td role="cell" class="centercell">1</td>
+                <td role="cell">Katona Bence</td>
+                <td role="cell">Cegléd, Csengeri szél 46.</td>
+                <td role="cell">+36309737274</td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="KP" id="check1" >
+                    <label class="layout" for="check1"></label>
+                </td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="BK" id="check2" >
+                    <label class="layout" for="check2"></label>
+                </td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="SZK" id="check3" >
+                    <label class="layout" for="check3"></label>
+                </td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="BAP" id="check4" >
+                    <label class="layout" for="check4"></label>
+                </td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="TART" id="check5" >
+                    <label class="layout" for="check5"></label>
+                </td>
+                <td role="cell" class="centercell">
+                <button type="submit" class="fizetve-button buttonhover">Fizetve</button>
+                </td>
+            </tr>
+
+            <tr role="row">
+                <td role="cell" class="centercell">2</td>
+                <td role="cell">Tóth Zsombor Gábor</td>
+                <td role="cell">Miskolc, Világ vége utca 26.</td>
+                <td role="cell">+36704657876</td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="KP" id="check1" >
+                    <label class="layout" for="check1"></label>
+                </td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="BK" id="check2" >
+                    <label class="layout" for="check2"></label>
+                </td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="SZK" id="check3" >
+                    <label class="layout" for="check3"></label>
+                </td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="BAP" id="check3" >
+                    <label class="layout" for="check3"></label>
+                </td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="TART" id="check3" >
+                    <label class="layout" for="check3"></label>
+                </td>
+                <td role="cell" class="centercell">
+                <button type="submit" class="fizetve-button buttonhover">Fizetve</button>
+                </td>
+            </tr>
+
+            <tr role="row">
+                <td role="cell" class="centercell">3</td>
+                <td role="cell">Kövesdi Hunor</td>
+                <td role="cell">Tápiószentmárton, Halál fasza 12 3.Em 14cs</td>
+                <td role="cell">+369987657</td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="KP" id="check1" >
+                    <label class="layout" for="check1"></label>
+                </td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="BK" id="check2" >
+                    <label class="layout" for="check2"></label>
+                </td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="SZK" id="check3" >
+                    <label class="layout" for="check3"></label>
+                </td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="BAP" id="check3" >
+                    <label class="layout" for="check3"></label>
+                </td>
+                <td role="cell" class="centercell">
+                    <input type="checkbox" class="check" name="TART" id="check3" >
+                    <label class="layout" for="check3"></label>
+                </td>
+                <td role="cell" class="centercell">
+                <button type="submit" class="fizetve-button buttonhover">Fizetve</button>
+                </td>
+
+            </tr>
+        </tbody>
+    </table>
+</div>
 </body>
