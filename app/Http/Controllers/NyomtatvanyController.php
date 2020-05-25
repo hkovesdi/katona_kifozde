a5b8cd5d144c5edee60aa722875b5297f75e26b1
hkovesdi <kovesdi.hunor99@gmail.com>
2020-05-25 10:40:21 +0200

    szakacs napi osszesito letrehozva

--- app/Http/Controllers/NyomtatvanyController.php
+++ app/Http/Controllers/NyomtatvanyController.php
@@ -11,4 +11,35 @@ class NyomtatvanyController extends Controller
     public function show() {
         return view('nyomtatvanyok');
     }
+
+
+    /**
+     * Szakácsoknak a napi összesítőt jeleníti meg (dátum, tételek neve + hány darab)
+     * @param string $datum - A dátum aminek a napi összesítője meg fog jelenni
+     */
+    public function showSzakacsView($datum) 
+    {
+        $tetelNevek = \App\TetelNev::all();
+        $parsedDatum = \Carbon\Carbon::parse($datum);
+
+        foreach($tetelNevek as $tetelNev) {
+            $tetelNev['darab'] = $tetelNev->tetelek()->whereHas('datum', function($query) {
+                $query->where('datum', $parsedDatum));
+            })->first()
+            ->megrendelesek()
+            ->count();
+        }
+
+        $datum = array("datum" => $datum, "het" => $parsedDatum->weekOfYear, "nap" => $parsedDatum->dayOfWeek);
+
+        //return view("nyomtatvanyok.szakacs", [
+          //  "tetelek" => $tetelNevek,
+         //   "datum" => $datum
+        //]);
+
+    }
+
+    
+
+
 }
