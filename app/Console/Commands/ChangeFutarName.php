388cd643e63af72707b1251c306b636ad58d09ae
hkovesdi <kovesdi.hunor99@gmail.com>
2021-01-26 00:04:57 +0100

    Create ChangeFutarNAme command

---
+++ app/Console/Commands/ChangeFutarName.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\User;
+use Illuminate\Console\Command;
+
+class ChangeFutarName extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'change:futar-name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        User::where('nev', 'Hunka Virág')->update(['nev' => 'Szalai Richárd']);
+        User::where('nev', 'Wéber Dominik')->update(['nev' => 'Wéber Tamás', 'munkakor' => 'Szakács']);
+    }
+}
